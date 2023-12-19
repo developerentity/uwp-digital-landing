7ab302cec54bb145ddb8d6c43493dc9084e4aaf7
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2023-12-19 22:16:28 +0200

    refactor: DialogCover component

--- src/components/DialogCover.tsx
+++ src/components/DialogCover.tsx
@@ -1,17 +1,28 @@
 'use client'
 
+import { useState } from "react"
+
 export default function DialogCover() {
 
-    const isOpen = false
-    const isVolume = false
+    const [isOpen, setIsOpen] = useState(false)
+    const [isMute, setIsMute] = useState(false)
     const flag = './flags/UA.svg'
+
+    const onOpen = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const onMute = () => {
+        setIsMute(!isMute)
+    }
 
     return (
         <div className='dialog-cover-bg  pt-3 overflow-hidden px-6 relative flex justify-between items-center'>
-
-            <button className='px-10 py-1 w-8 h-8 rounded-full border-2 border-gray-500 flex items-center justify-center text-gray-500 hover:border-gray-200 hover:text-gray-200 transition duration-300'>
-          
-                on
+            <button
+                onClick={onMute}
+                className='dialog-cover-btn w-20  py-2 px-2.5 uppercase    justify-between text-gray-500'>
+                <img className="h-5" src={isMute ? './muted.svg' : './unmuted.svg'} alt="is muted" />
+                <div className="">{isMute ? 'off' : 'on'}</div>
             </button>
 
             <div className='absolute z-10 transform -translate-x-1/2 left-1/2 top-2.5'>
@@ -19,11 +30,13 @@ export default function DialogCover() {
             </div>
 
             <div className='flex justify-between'>
-                <button className='w-8 h-8 p-1.5 rounded-full border-2 border-gray-500 flex items-center justify-center hover:border-gray-200 hover:text-gray-200 transition duration-300'>
-                    <img src={isOpen ? './arrow-down.svg' : './arrow-up.svg'} alt="arrow-down" />
+                <button
+                    onClick={onOpen}
+                    className='dialog-cover-btn w-8 p-1.5    justify-center'>
+                    <img className="h-5" src={isOpen ? './arrow-down.svg' : './arrow-up.svg'} alt="arrow" />
                 </button>
-                <button className='w-8 h-8 rounded-full border-2 border-gray-500 flex items-center justify-center overflow-hidden ms-3 hover:border-gray-200 hover:text-gray-200 transition duration-300'>
-                    <img src={flag} alt="arrow-down" />
+                <button className='dialog-cover-btn w-8    justify-center overflow-hidden ms-3'>
+                    <img  src={flag} alt="flag" />
                 </button>
             </div>
         </div>
