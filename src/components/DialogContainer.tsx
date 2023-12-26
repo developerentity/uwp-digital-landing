7f1878c0f33ee09cebfaa3a94c0f31f1684fa59f
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2023-12-26 17:46:17 +0200

    style: minor layout changes in DialogContainer was made

--- src/components/DialogContainer.tsx
+++ src/components/DialogContainer.tsx
@@ -28,17 +28,16 @@ export default function DialogContainer() {
     return (
         <div className={`chat-container ${isExpanded ? 'expanded' : ''}`}>
             <DialogCover isExpanded={isExpanded} handleChatClick={handleChatClick} />
-            <div className="bg-gradient-to-b from-[#000000] to-[#171717] border border-solid border-[#8B5CF6] flex-1 mt-9 max-[480px]:rounded-b-[18px] min-[480px]:rounded-[18px]">
-                <div className='px-5 pt-10'>
-                    <div>
+            <div className="bg-gradient-to-b from-[#000000] to-[#171717] border border-solid border-[#8B5CF6] flex-1 mt-9 max-[480px]:rounded-b-[18px] min-[480px]:rounded-[18px] overflow-hidden">
+                <div className='px-5 pb-3 sm:pb-5 flex flex-col justify-end h-full'>
+                    <div >
                         MESSAGES
                     </div>
-                    {/* <div className="flex-1">
+                    <div className="">
                         <div className="input-container flex justify-between bg-[#404040] py-2 px-3 rounded-xl">
-                            <button className="p-1 w-[52px] h-[52px] bg-[#8B5CF6] rounded-xl flex justify-center items-center">
+                            <button className="p-1 h-[52px] bg-[#8B5CF6] rounded-xl flex justify-center items-center">
                                 <img className="w-[2rem]" src='./paperclip.svg' />
                             </button>
-                            <textarea className="w-[77%] text-lg p-2 max-h-[1642px]   bg-[#171717] rounded-xl h-150 min-h-150 resize-y overflow-auto" />
                             <textarea
                                 value={text}
                                 onChange={handleChange}
@@ -49,7 +48,7 @@ export default function DialogContainer() {
                                 <img className="w-[1.5rem]" src='./mic.svg' />
                             </button>
                         </div>
-                    </div> */}
+                    </div>
                 </div>
             </div>
         </div>
