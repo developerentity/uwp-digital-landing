a0963d8d82cc69d45ece6edc765c752e561fd2a6
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-01-07 17:23:17 +0200

    feat/style: Add File Button was created

---
+++ src/components/AddFileButton.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { ChangeEvent } from "react";
+
+
+export default function AddFileButton({ handleFileChange }: IProps) {
+
+    return (
+        <div>
+            <label htmlFor="fileInput" className="px-1 w-[28px] h-[28px] rounded-xl flex justify-center items-center cursor-pointer">
+                <Image src='./paperclip.svg' alt='Paperclip' width={36} height={28} />
+            </label>
+            <input
+                type="file"
+                id="fileInput"
+                className="hidden"
+                onChange={handleFileChange}
+            />
+        </div>
+    )
+}
+
+interface IProps {
+    handleFileChange: (event: ChangeEvent<HTMLInputElement>) => void
+}
